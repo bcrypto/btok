5c8cc02fcda064358ff6f7f2e2f8d8a303cd7c29
MaryAlex <maria.chmyreva@gmail.com>
2019-07-24 23:23:46 +0300

    Added high level description of tokens states.

--- model/states/exports.py
+++ model/states/exports.py
@@ -1,6 +1,7 @@
 from sismic.io import export_to_yaml, export_to_plantuml
 
-from passwords_statechart import statechart
+from statecharts.crypto_token_statechart import crypto_token_statechart
+from statecharts.passwords_statechart import passwords_statechart
 
-export_to_yaml(statechart, 'passwordBTOK.yml')
-# print(export_to_plantuml(statechart))
+export_to_yaml(crypto_token_statechart, 'statecharts/results/cryptoTokenStates.yml')
+# print(export_to_plantuml(crypto_token_statechart))
--- model/states/tests.py
+++ model/states/tests.py
@@ -3,27 +3,36 @@ import random
 from sismic.interpreter import Interpreter
 from sismic.io import import_from_yaml
 
-elevator = import_from_yaml(filepath='passwordBTOK.yml')
-interpreter = Interpreter(elevator)
-print('Before:', interpreter.configuration)
-step = interpreter.execute_once()
-print('Step: ', step)
-print('Configuration: ', interpreter.configuration)
-print('Context: ', interpreter.context)
-while step:
-    print('_______________________________')
-    events = elevator.events_for(interpreter.configuration)
-    print('Events: ', events)
-    if events:
-        next_event = random.randrange(start=0, stop=len(events))
-        print('Next event(!!!!!): ', events[next_event])
-        interpreter.queue(events[next_event])
+from statecharts.crypto_token_statechart import crypto_token_statechart
+from statecharts.passwords_statechart import passwords_statechart
+
+
+def random_tests(elevator, has_error_in_context):
+    interpreter = Interpreter(elevator)
+    print('Before:', interpreter.configuration)
     step = interpreter.execute_once()
     print('Step: ', step)
     print('Configuration: ', interpreter.configuration)
     print('Context: ', interpreter.context)
-    if interpreter.context['pin_count'] < 0:
-        print('ERRORR!')
-        exit(1)
+    while step:
+        print('_______________________________')
+        events = elevator.events_for(interpreter.configuration)
+        print('Events: ', events)
+        if events:
+            next_event = random.randrange(start=0, stop=len(events))
+            print('Next event(!!!!!): ', events[next_event])
+            interpreter.queue(events[next_event])
+        step = interpreter.execute_once()
+        print('Step: ', step)
+        print('Configuration: ', interpreter.configuration)
+        print('Context: ', interpreter.context)
+        if has_error_in_context(interpreter):
+            print('ERRORR!')
+            exit(1)
 
-print('After:', interpreter.configuration)
+    print('After:', interpreter.configuration)
+
+
+# random_tests(passwords_statechart,
+#              lambda interpreter: interpreter.context['pin_count'] < 0)
+random_tests(crypto_token_statechart, lambda interpreter: False)
