c175a4c274d80ec9e822bc52434db18417e8d166
MaryAlex <maria.chmyreva@gmail.com>
2020-06-09 16:49:15 +0300

    Added Readme.md.

--- model/states/exports.py
+++ model/states/exports.py
@@ -4,5 +4,7 @@ from statecharts.crypto_token_statechart import crypto_token_statechart
 from statecharts.e_sign import e_sign_statechart
 from statecharts.passwords_statechart import passwords_statechart
 
+# Код выгрузки состояния eSign в файл eSignStates.yml в папке results.
 export_to_yaml(e_sign_statechart, 'statecharts/results/eSignStates.yml')
-# print(export_to_plantuml(e_sign_statechart))
+# Экспорт состояния eSign в код PlantUML для дальнейшей визуализации.
+print(export_to_plantuml(e_sign_statechart))
